5d7268fe4aba5f49f839e1f69b974da638775b31
Ztoyj <helenzheng1201@gmail.com>
2025-02-28 01:00:47 -0500

    Implemented test double.

--- src/main/java/AndrewWebServices/InMemoryDatabase.java
+++ src/main/java/AndrewWebServices/InMemoryDatabase.java
@@ -1,12 +1,23 @@
 package AndrewWebServices;
 
+import java.util.HashMap;
+import java.util.Map;
 /*
  * InMemoryDatabase is a fake for the AndrewWS database which is used to improve test efficiency.
  * Remember, fakes are fully functional classes with simplified implementation.
  * What is the simplest core functionality that we need for a functional database?
- * 
+ *
  * Hint: there are two methods you need to implement
  */
-public class InMemoryDatabase /* should there be something here? */ {
-    // Implement your fake database here
+public class InMemoryDatabase extends Database {
+    private final Map<String, Integer> data = new HashMap<>();
+
+    public InMemoryDatabase() {
+        data.put("Scotty", 17214);
+    }
+
+    @Override
+    public int getPassword(String accountName) {
+        return data.getOrDefault(accountName, -1);
+    }
 }
--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -2,9 +2,11 @@ package AndrewWebServices;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class AndrewWebServicesTest {
     Database database;
@@ -15,9 +17,10 @@ public class AndrewWebServicesTest {
     @Before
     public void setUp() {
         // You need to use some mock objects here
-        database = new Database(); // We probably don't want to access our real database...
-        recommender = new RecSys();
-        promoService = new PromoService();
+        database = new InMemoryDatabase();
+        recommender = mock(RecSys.class);
+        when(recommender.getRecommendation("Scotty")).thenReturn("Animal House");
+        promoService = mock(PromoService.class);
 
         andrewWebService = new AndrewWebServices(database, recommender, promoService);
     }
@@ -36,13 +39,12 @@ public class AndrewWebServicesTest {
 
     @Test
     public void testSendEmail() {
-        // How should we test sendEmail() when it doesn't have a return value?
-        // Hint: is there something from Mockito that seems useful here?
+        andrewWebService.sendPromoEmail("test@example.com");
+        verify(promoService).mailTo("test@example.com");
     }
 
     @Test
     public void testNoSendEmail() {
-        // How should we test that no email has been sent in certain situations (like right after logging in)?
-        // Hint: is there something from Mockito that seems useful here?
+        verify(promoService, never()).mailTo(anyString());
     }
 }
